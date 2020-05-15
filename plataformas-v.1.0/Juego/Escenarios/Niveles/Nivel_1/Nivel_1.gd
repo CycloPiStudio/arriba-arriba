d5a0f9ef42ba125f21639888dfbfa752dfb11b1e
juanantoniogit <juanantonio.tecno@gmail.com>
2020-05-15 17:34:35 +0200

    nivel 1

--- plataformas-v.1.0/Juego/Escenarios/Niveles/Nivel_1/Nivel_1.gd
+++ plataformas-v.1.0/Juego/Escenarios/Niveles/Nivel_1/Nivel_1.gd
@@ -1,35 +1,33 @@
 extends Node2D
 
-onready var tilemap 
-onready var personaje 
+onready var tilemap : TileMap 
+onready var tileset : TileSet = preload("res://recursos/TileSet/tileset-escenario.tres")
+onready var personaje : KinematicBody2D = preload("res://Juego/Caracteres/Player/Player_01/Player_01.tscn").instance()
 onready var enemigo_02 = preload("res://Juego/Caracteres/Enemigos/Enemigo_02/Enemigo_02.tscn").instance()
 onready var banderaAltura = preload("res://Juego/HUD/bandera de altura/bandera de altura.tscn").instance()
 
-func _process(_delta):
-
-	set_camera_limits()
-
-
-
-func set_camera_limits():
-    var map_limits = $TileMap.get_used_rect()
-    var map_cellsize = $TileMap.cell_size
-    $Player/Camera2D.limit_left = map_limits.position.x * map_cellsize.x
-    $Player/Camera2D.limit_right = map_limits.end.x * map_cellsize.x
-    $Player/Camera2D.limit_top = map_limits.position.y * map_cellsize.y
-    $Player/Camera2D.limit_bottom = map_limits.end.y * map_cellsize.y
-
-
-
-#= $TileMap
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("/root/Level").add_child(enemigo_02)
 	get_node("/root/Level").add_child(banderaAltura)
-#	get_node("/root/Principal").add_child(personaje)
-#	get_node("/root/Principal/Player").set_position(Vector2(get_viewport().size.x/2, 150))
-	tilemap = $TileMap
+
 	personaje = $Player
+	get_node("/root/Level").add_child(personaje)
+	get_node("/root/Level/Player").set_position(Vector2(get_viewport().size.x/2, 150))
+	tilemap = $TileMap
+	tilemap = TileMap.new ()
+#	tilemap.MODE_SQUARE # esto estaba comentado
+#	tilemap.cell_size # esto estaba comentado
+	tilemap.set_cell_size(Vector2(32,32)) 
+	
+#	tileset = TileSet.new()
+	tilemap.set_tileset(tileset) 
+	get_node("/root/Level").add_child(tilemap)
+#	var tileMapName = get_node("/root/Level").get_child(1).name
+#	get_node("/root/Level/" + str(tileMapName)).set_name("escenario")
+	for i in 200:
+		tilemap.set_cell(i, 7, 10)
+	
+
 
 
 func _input(event):
@@ -41,7 +39,7 @@ func _input(event):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
 #		var mouse_pos = get_viewport().get_mouse_position()
 		var personaje_pos = Vector2(personaje.get_position().x + lado, personaje.get_position().y)
-		
+
 		var tile_pos = tilemap.world_to_map(personaje_pos)
 		tilemap.set_cell(tile_pos.x,tile_pos.y, 1)
 		#print(tile_pos)
