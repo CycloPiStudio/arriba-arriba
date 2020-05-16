1bc0521fe2c3e5a7e79210ce56dafdd5582667f1
juanantoniogit <juanantonio.tecno@gmail.com>
2020-05-16 20:14:41 +0200

    player

--- plataformas-v.1.0/Juego/Caracteres/Player/Player_01/Player_01.gd
+++ plataformas-v.1.0/Juego/Caracteres/Player/Player_01/Player_01.gd
@@ -1,32 +1,25 @@
 
-##############
 extends KinematicBody2D
 
+
+onready var tilemap = get_parent().get_node("TileMap")
 onready var sprite : AnimatedSprite = $AnimatedSprite
 #onready var animation_player : AnimationPlayer = $AnimationPlayer
 #onready var audio_player : AudioStreamPlayer = $AudioStreamPlayer2D
+#var analog_velocity = Vector2(0,0)
 export var snap := false
-export var move_speed := 240
-export var jump_force := 200
-export var gravity := 300
+export var move_speed := 250
+export var jump_force := 500
+export var gravity := 900
 export var slope_slide_threshold := 50.0
-#var vidas_personaje = 3
+var vidas_personaje = 3
+var vida = 100
 var velocity := Vector2()
-#var conesion_anima_fin
-#onready var tilemap = get_node("/root/Level/TileMap")
-#var left = -0.5
-#var right = 0.5
-#var direction
 
-#Cyclo Pi variables
-#var bandera_boton_pulsao = false
-onready var gameover = load("res://Menus/GameOver/GameOver.tscn").instance()
-var bandera_muerto = false
-var bandera_sonido_pasos = true
-var tilemap
 func _ready():
-	tilemap = get_parent().get_node("tilemap")
-	print(tilemap)
+
+	pass
+	
 
 func _physics_process(delta: float) -> void:
 	
@@ -37,7 +30,7 @@ func _physics_process(delta: float) -> void:
 		velocity.y = -jump_force
 		snap = false
 #		audio_player.play()
-	
+
 	velocity.y += gravity * delta
 
 	var snap_vector = Vector2(0,32) if snap else Vector2()
@@ -45,9 +38,6 @@ func _physics_process(delta: float) -> void:
 
 	if is_on_floor() and (Input.is_action_just_released("move_right") or Input.is_action_just_released("move_left")):
 		velocity.y = 0
-	
-	if not is_on_floor():
-		snap = false
 
 	var just_landed := is_on_floor() and not snap
 	if just_landed:
@@ -60,85 +50,42 @@ func _physics_process(delta: float) -> void:
 	
 	update_animation(velocity)
 
+func quitarVida():
+	var menosVida = 10
+	vida = vida - menosVida
+	print(vida)
+	if vida <= 0:
+		vidas_personaje -= 1
+		print("una vida menos")
+		get_parent().queue_free()
+
+		if vidas_personaje <= 0:
+			print("se acabo la partida")
+	
 func update_animation(velocity: Vector2) -> void:
-	var animation := "caminar"
+#	var animation := "idle"
 	
 	if abs(velocity.x) > 10.0:
-#		
+#		var estaflip = $Sprite.is_flipped_h()
 		sprite.flip_h = velocity.x < 0
-		animation = "caminar"
-		if bandera_sonido_pasos:
-			bandera_sonido_pasos = false
-			get_node("AudioPasos").play()
-			
-
-	if not is_on_floor():
-		animation = "saltar" if velocity.y < 0 else "caer"
 		
-	if sprite.animation != animation:
-		sprite.play(animation)
-
-
-#func _on_TextureButton_pressed():
-#	bandera_boton_pulsao = false
-#
-#func _on_TextureButton_button_down():
-#	bandera_boton_pulsao = true
-#	if snap :
-#		salto()
-	
-func salto():
-	snap = false
-	velocity.y = -jump_force
-	get_node("AudioSalto").play()
-
-
-func _on_AudioPasos_finished():
-	if snap :
-		bandera_sonido_pasos = true
-	pass # Replace with function body.
-	
-func morir():
-	bandera_muerto = true
-	Global.puntuacion = 0
-	Global.monedas = 0
-	
-	quitar_vida()
-#	print("game over por límite inferior, esto hay que cambiarlo por game over por areas")
-
-#	gameover.set_position(get_position())
-#	Global_cambiar_nivel.goto_scene("res://Menus/GameOver/GameOver.tscn")
-	pass
-	
-func quitar_vida():
-	if Global.vidas > 1:
-		Global.vidas -= 1
-#		Global_cambiar_nivel.wait_frames = 20
-		Global_cambiar_nivel.goto_scene(get_parent().filename)
-	elif Global.vidas <= 1 :
-		Global.vidas -= 1
-#		Global_cambiar_nivel.wait_frames = 0
-#		Global_cambiar_nivel.goto_scene("res://Menus/GameOver/GameOver.tscn")
-		get_parent().add_child(gameover)
-#		add_child(gameover)
-
 func _input(event):
 	var lado = 32
 	if get_node("AnimatedSprite").flip_h:
 		lado = -32
 	else:
 		lado = 32
-	
-	if event is InputEventMouseButton :
-
+		
+#	Poner bloques
+	if Input.is_action_just_pressed("ui_select") or event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
 #		var mouse_pos = get_viewport().get_mouse_position()
 		var personaje_pos = Vector2(get_position().x + lado, get_position().y)
 		var tile_pos = tilemap.world_to_map(personaje_pos)
-		if event.button_index == BUTTON_LEFT:
-		
-			tilemap.set_cell(tile_pos.x,tile_pos.y, 1)
-		
-		if event.button_index == BUTTON_RIGHT:
-#	
-			tilemap.set_cell(tile_pos.x,tile_pos.y, -1)
-#		
+		tilemap.set_cell(tile_pos.x,tile_pos.y, 1)
+		#print(tile_pos)
+	if Input.is_action_just_pressed("ui_accept") or event is InputEventMouseButton and event.button_index == BUTTON_RIGHT:
+#		var mouse_pos = get_viewport().get_mouse_position()
+		var personaje_pos = Vector2(get_position().x + lado , get_position().y)
+		var tile_pos = tilemap.world_to_map(personaje_pos)
+		tilemap.set_cell(tile_pos.x,tile_pos.y, -1)
+#		print(tile_pos)
