838d362a7c780eb7a08d4161c1cde00a59375158
brecabre <bravomantonio@gmail.com>
2020-05-16 19:52:15 +0200

    cambio nivel 1

--- plataformas-v.1.0/Juego/Escenarios/Niveles/Nivel_1/Nivel_1.gd
+++ plataformas-v.1.0/Juego/Escenarios/Niveles/Nivel_1/Nivel_1.gd
@@ -7,45 +7,21 @@ onready var enemigo_02 = preload("res://Juego/Caracteres/Enemigos/Enemigo_02/Ene
 onready var banderaAltura = preload("res://Juego/HUD/bandera de altura/bandera de altura.tscn").instance()
 
 func _ready():
+	
+	tilemap = TileMap.new ()
+	tilemap.set_name("tilemap")
+	tilemap.set_cell_size(Vector2(32,32)) 
+	tilemap.set_tileset(tileset) 
+	get_node("/root/Level").add_child(tilemap)
 	get_node("/root/Level").add_child(enemigo_02)
 	get_node("/root/Level").add_child(banderaAltura)
 
-	personaje = $Player
 	get_node("/root/Level").add_child(personaje)
 	get_node("/root/Level/Player").set_position(Vector2(get_viewport().size.x/2, 150))
-	tilemap = $TileMap
-	tilemap = TileMap.new ()
-#	tilemap.MODE_SQUARE # esto estaba comentado
-#	tilemap.cell_size # esto estaba comentado
-	tilemap.set_cell_size(Vector2(32,32)) 
-	
-#	tileset = TileSet.new()
-	tilemap.set_tileset(tileset) 
-	get_node("/root/Level").add_child(tilemap)
-#	var tileMapName = get_node("/root/Level").get_child(1).name
-#	get_node("/root/Level/" + str(tileMapName)).set_name("escenario")
+
 	for i in 200:
-		tilemap.set_cell(i, 7, 10)
+		tilemap.set_cell(i, 7, 1)
 	
 
 
 
-func _input(event):
-	var lado = 32
-	if personaje.get_node("AnimatedSprite").flip_h:
-		lado = -32
-	else:
-		lado = 32
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
-#		var mouse_pos = get_viewport().get_mouse_position()
-		var personaje_pos = Vector2(personaje.get_position().x + lado, personaje.get_position().y)
-
-		var tile_pos = tilemap.world_to_map(personaje_pos)
-		tilemap.set_cell(tile_pos.x,tile_pos.y, 1)
-		#print(tile_pos)
-	if event is InputEventMouseButton and event.button_index == BUTTON_RIGHT:
-#		var mouse_pos = get_viewport().get_mouse_position()
-		var personaje_pos = Vector2(personaje.get_position().x + lado ,personaje.get_position().y)
-		var tile_pos = tilemap.world_to_map(personaje_pos)
-		tilemap.set_cell(tile_pos.x,tile_pos.y, -1)
-#		print(tile_pos)
