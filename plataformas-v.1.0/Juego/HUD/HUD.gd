82dba780a20d68545402d15ff906105a3e458c4c
juanantoniogit <juanantonio.tecno@gmail.com>
2020-05-19 13:20:36 +0200

    pongo los controles en el HUD

--- plataformas-v.1.0/Juego/HUD/HUD.gd
+++ plataformas-v.1.0/Juego/HUD/HUD.gd
@@ -5,6 +5,7 @@ extends CanvasLayer
 #func puntos (puntos):
 #	$puntuacion.set_text(puntos) 
 func _process(_delta):
+
 #	get_node("Score/puntuacion").set_text(str(Global.puntuacion))
 	if Global.vidas == 2:
 		get_node("vidas/vida3").set_visible(false)
@@ -16,3 +17,13 @@ func _process(_delta):
 		get_node("vidas/vida3").set_visible(false)
 		get_node("vidas/vida2").set_visible(false)
 		get_node("vidas/vida1").set_visible(false)
+	
+	if Global.touch == true:
+		get_node("touch").set_visible(true)
+	if Global.touch == false:
+		get_node("touch").set_visible(false)
+	
+
+func _on_Salir_button_down():
+	Global_cambiar_nivel.goto_scene("res://Menus/MenuPrincipal/MenuPrincipal.tscn")
+	pass # Replace with function body.
