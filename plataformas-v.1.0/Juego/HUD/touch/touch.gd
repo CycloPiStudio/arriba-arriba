4c45f9828c857e3c54a3229ef086200f7032ed6f
juanantoniogit <juanantonio.tecno@gmail.com>
2020-05-19 16:57:34 +0200

    termino movimientos del touch

--- plataformas-v.1.0/Juego/HUD/touch/touch.gd
+++ plataformas-v.1.0/Juego/HUD/touch/touch.gd
@@ -1,24 +1,44 @@
 extends Node2D
 
+
 onready var player = get_node("/root/Level/Player")
+var dere = 0
+var izq = 0
 
 func _on_botonizq_button_down():
-
-	pass # Replace with function body.
+	izq = 1
+	player.direction_x = dere - izq
+	player.movimiento()
+	pass
+func _on_botonizq_button_up():
+	izq = 0
+	player.direction_x = dere - izq
+	pass
 	
 func _on_botondere_button_down():
-
-	pass # Replace with function body.
+	dere = 1
+	player.direction_x = dere - izq
+	player.movimiento()
+	pass
+func _on_botondere_button_up():
+	dere = 0
+	player.direction_x = dere - izq
+	pass
 	
 func _on_botonarriba_button_down():
 	if player.snap == true:
 		player.saltar()
-	pass # Replace with function body.
+	pass
 
 func _on_botonA_button_down():
-	Input.is_action_just_pressed("ui_select")
-	pass # Replace with function body.
+	player.ponerQuitarBloque(1,1,1)
+	pass
 
 func _on_botonB_button_down():
-	Input.is_action_just_pressed("ui_accept")
-	pass # Replace with function body.
+	player.ponerQuitarBloque(1,1,-1)
+	pass
+
+
+
+
+
