cfbf2cdfea18c053e6115898e2b7b9a65497d263
juanantoniogit <juanantonio.tecno@gmail.com>
2020-05-19 13:18:40 +0200

    pongo menu controles y su conexion con principal

--- plataformas-v.1.0/Menus/MenuPrincipal/MenuPrincipal.gd
+++ plataformas-v.1.0/Menus/MenuPrincipal/MenuPrincipal.gd
@@ -5,8 +5,8 @@ func _ready():
 	get_node("titulo").set_position(Vector2(get_viewport().size.x/2, get_viewport().size.y/2))
 	pass # Replace with function body.
 
-func _on_TextureButton_button_down():
-	comenzarJuego()
+
+
 
 func _input(_event):
 	if Input.is_action_just_pressed("ui_select"):
@@ -15,3 +15,12 @@ func _input(_event):
 func comenzarJuego():
 		Global.nivel = 0
 		Global_cambiar_nivel.goto_scene("res://Juego/Escenarios/Niveles/Nivel_1/Nivel_1.tscn")
+
+func _on_TextureButtonPlay_button_down():
+	comenzarJuego()
+	pass # Replace with function body.
+
+
+func _on_TextureButtonOption_button_down():
+	Global_cambiar_nivel.goto_scene("res://Menus/Controles/controles.tscn")
+	pass # Replace with function body.
