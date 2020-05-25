f17a226b509d3f22666bd336bb81bbd737114254
juanantoniogit <juanantonio.tecno@gmail.com>
2020-05-25 17:07:54 +0200

    Update Nivel_1.gd

--- plataformas-v.1.0/Juego/Escenarios/Niveles/Nivel_1/Nivel_1.gd
+++ plataformas-v.1.0/Juego/Escenarios/Niveles/Nivel_1/Nivel_1.gd
@@ -12,7 +12,7 @@ func _ready():
 	for i in 200:
 		tilemap.set_cell(i, 7, 10)
 	personaje = $Player
-	get_node("/root/Level/Player").set_position(Vector2(get_viewport().size.x/2, 150))
+	get_node("Player").set_position(Vector2(get_viewport().size.x/2, 150))
 
 
 
