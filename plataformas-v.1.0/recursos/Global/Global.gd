240d4e5e1660c971b2168a9cbac875e529f4b63d
juanantoniogit <juanantonio.tecno@gmail.com>
2020-05-28 16:55:05 +0200

    Update Global.gd

--- plataformas-v.1.0/recursos/Global/Global.gd
+++ plataformas-v.1.0/recursos/Global/Global.gd
@@ -5,8 +5,8 @@ var puntuacion = 0
 var monedas = 0
 var vidas = vidasIniciales
 var nivel = 1
-#var carpeta_niveles = "res://Juego/Escenarios/Niveles"
-#var numero_max_niveles : int
+var carpeta_niveles = "res://Juego/Escenarios/Niveles"
+var numero_max_niveles : int
 var touch = true
 
 func _ready():
@@ -15,28 +15,28 @@ func _ready():
 	print("vidas: " + str(vidas))
 	print("nivel: " + str(nivel))
 	print("touch: " + str(touch))
-#	numero_max_niveles = (lista_archivos_en_directorio(carpeta_niveles).size())
+	numero_max_niveles = (lista_archivos_en_directorio(carpeta_niveles).size())
 
 #func _process(delta):
 #	print(vidas)
 
-#func lista_archivos_en_directorio(path):
-#    var files = []
-#    var dir = Directory.new()
-#    dir.open(path)
-#    dir.list_dir_begin()
-#
-#    while true:
-#        var file = dir.get_next()
-#        if file == "":
-#            break
-#        elif not file.begins_with("."):
-#            files.append(file)
-#
-#    dir.list_dir_end()
-#
-#    return files
-#func _unhandled_input(event):
-#    if event is InputEventKey:
-#        if event.pressed and event.scancode == KEY_ESCAPE:
-#            get_tree().quit()
+func lista_archivos_en_directorio(path):
+    var files = []
+    var dir = Directory.new()
+    dir.open(path)
+    dir.list_dir_begin()
+
+    while true:
+        var file = dir.get_next()
+        if file == "":
+            break
+        elif not file.begins_with("."):
+            files.append(file)
+
+    dir.list_dir_end()
+
+    return files
+func _unhandled_input(event):
+    if event is InputEventKey:
+        if event.pressed and event.scancode == KEY_ESCAPE:
+            get_tree().quit()
